b93a86202fe8198469ec460088febc1fde26a855
PokeFred <Cedric.paelmke@gmx.de>
2025-05-09 13:15:51 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,30 @@
-<div>Hello World</div>
+<script lang="ts">
+    import { onMount } from "svelte"
+
+    let lat: number | undefined = undefined
+    let lon: number | undefined = undefined
+    let acc: number | undefined = undefined
+
+    onMount((): void => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                lat = position.coords.latitude
+                lon = position.coords.longitude
+                acc = position.coords.accuracy
+            },
+            (error) => {
+                console.log(error.message)
+            }
+        )
+    })
+</script>
+
+<div>Werte</div>
+<div>Latitude: {#if lat !== undefined} {lat} {/if}</div>
+<div>Longitude: {#if lon !== undefined} {lon} {/if}</div>
+<div>Accuracy: {#if acc !== undefined} {acc} {/if}</div>
+<br />
+<div>Erklärungen</div>
+<div><span style="font-weight: 700;">Latitude</span>: Breitengrad (Nord/Süd)</div>
+<div><span style="font-weight: 700;">Longitude</span>: Längengrad (Ost/West)</div>
+<div><span style="font-weight: 700;">Accuracy</span>: Genauigkeit der Position in Metern</div>
